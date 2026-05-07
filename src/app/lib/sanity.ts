537a400bc28abcc3bf663b2cdeedf5dd5d77be76
waxgaby2 <waxgabey@gmail.com>
2026-05-07 18:47:37 +0100

    move sanity config to env variables

--- src/app/lib/sanity.ts
+++ src/app/lib/sanity.ts
@@ -1,9 +1,8 @@
-
 import { createClient } from "@sanity/client"
 
 export const client = createClient({
-  projectId: "yc8n7g05",
-  dataset: "production",
-  apiVersion: "2024-01-01",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
   useCdn: true,
 })
